e0be8c5dc83b6d9b84d13e6fbfbe2ea60e069875
ronaldo <ronaldo@g.globo>
2020-12-08 10:08:50 -0300

    Change setup.py version

--- setup.py
+++ setup.py
@@ -5,17 +5,15 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mongo-system-log",
-    version="0.0.6",
+    version="0.0.8",
     author="Ronaldo Duarte",
     author_email="ronaldoduarte@globo.com",
     description="A log package that works with MongoDB.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    project_urls={
-        "Source Code": "https://github.com/ronaldodduarte/mongo-system-log"
-    },
-    install_requires=['pymongo'],
-    packages=['mongo_system_log'],
+    url="https://github.com/ronaldodduarte/mongo_system_log",
+    install_requires=["pymongo"],
+    packages=["mongo_system_log"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
