f702802d6f80afc7a3c8e4f5d84c35022483bb85
ronaldo <ronaldo@g.globo>
2020-12-11 09:56:18 -0300

    [FIX] Changed version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mongo-system-log",
-    version="0.1.0",
+    version="0.1.1",
     author="Ronaldo Duarte",
     author_email="ronaldoduarte@globo.com",
     description="A log package that works with MongoDB.",
